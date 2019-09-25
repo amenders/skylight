5e01ec24a77589fabd6edcd82bd23b632c53494b
Amos Endersen <amenders@iupui.edu>
2019-09-24 21:51:54 -0400

    moved services from project to library, and filled out AsteroidService unit tests

--- Skylight.Test/AsteroidServiceTest.cs
+++ Skylight.Test/AsteroidServiceTest.cs
@@ -4,19 +4,26 @@ using System;
 namespace Skylight.Test
 {
     [TestFixture()]
-    public class Test
+    public class AsteroidServiceTest
     {
-        [Test()]
-        public void TestCase()
-        {
-            Assert.True(true);
+        private AsteroidService asteroids = new AsteroidService();
 
+        [Test()]
+        public void Throw_Exception_When_Start_Date_Before_End_Date()
+        {
+            var start = new DateTime(2019, 1, 10);
+            var end = new DateTime(2019, 1, 2);
+
+            Assert.Throws<InvalidAsteroidParametersException>(async () => await asteroids.GetAsteroidByDateAsync(start, end));
         }
 
         [Test()]
-        public void Pass()
+        public void Throw_Exception_When_Date_Range_Greater_Than_Seven()
         {
-            Assert.True(true);
+            var start = new DateTime(2019, 1, 10);
+            var end = new DateTime(2019, 1, 20);
+
+            Assert.Throws<InvalidAsteroidParametersException>(async () => await asteroids.GetAsteroidByDateAsync(start, end));
         }
     }
 }
